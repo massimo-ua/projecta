dca74878952bd88efc0067bf9066c82511a34ba1
maksym.hryshkov <grishkov.m@gmail.com>
2024-12-27 20:36:42 +0200

    chore: fix projects page layout

--- web-ui/src/components/Projects/ProjectCard.jsx
+++ web-ui/src/components/Projects/ProjectCard.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Typography } from 'antd';
 import { Link } from 'react-router-dom';
 
 const { Meta } = Card;
+const { Paragraph } = Typography;
 
 export function ProjectCard({ project }) {
   return (
     <div className="ProjectCard_container">
-      <Card bordered style={{ minWidth: 300 }}>
-        <Meta title={<Link title={project.name} to={`/projects/${project.id}`}>{project.name}</Link>} description={project.description} />
+      <Card bordered style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <Meta
+          title={
+            <Link title={project.name} to={`/projects/${project.id}`}>
+              <Paragraph ellipsis={{ rows: 1 }} style={{ marginBottom: 0 }}>
+                {project.name}
+              </Paragraph>
+            </Link>
+          }
+          description={
+            <Paragraph ellipsis={{ rows: 2 }} type="secondary">
+              {project.description}
+            </Paragraph>
+          }
+        />
       </Card>
     </div>
   );
