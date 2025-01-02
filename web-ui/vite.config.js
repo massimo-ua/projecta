6ed029faf152e1cad0e84f2fbdd8d7601b4c3d72
maksym.hryshkov <grishkov.m@gmail.com>
2025-01-03 01:07:27 +0200

    chore: update vite config

--- web-ui/vite.config.js
+++ web-ui/vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: true,
+    uglify: true,
+    minify: true,
+    reportCompressedSize: true,
+  },
   server: {
     proxy: {
       '/api': {
