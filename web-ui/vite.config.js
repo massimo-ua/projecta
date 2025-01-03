09e83abe4bbb216d51716b0d3c6e608f8f746362
maksym.hryshkov <grishkov.m@gmail.com>
2025-01-03 21:17:28 +0200

    chore: update build config

--- web-ui/vite.config.js
+++ web-ui/vite.config.js
@@ -3,12 +3,45 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react() ],
   build: {
-    cssMinify: true,
-    uglify: true,
-    minify: true,
-    reportCompressedSize: true,
+    minify: 'esbuild',
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            '@ant-design/icons',
+            '@react-oauth/google',
+            'antd',
+            'date-fns',
+            'dayjs',
+            'jwt-decode',
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'styled-components',
+          ],
+        },
+        chunkFileNames: 'assets/[hash].js',
+        entryFileNames: 'assets/[hash].js',
+        assetFileNames: 'assets/[hash].[ext]',
+        compact: true,
+      }
+    },
+    cssCodeSplit: true,
+    sourcemap: false,
+    chunkSizeWarningLimit: 500,
+    assetsInlineLimit: 4096,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      },
+      format: {
+        comments: false,
+      }
+    },
   },
   server: {
     proxy: {
